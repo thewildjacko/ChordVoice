792817f2bd947e462ca83d46875913f234c1f642
Jake Smolowe <jakesmolowe@gmail.com>
2018-02-11 11:28:06 -0800

    Added Swifter extensions

---
+++ ChordVoice/Extensions/Foundation/URLExtensions.swift
@@ -0,0 +1,59 @@
+//
+//  URLExtensions.swift
+//  SwifterSwift
+//
+//  Created by Omar Albeik on 03/02/2017.
+//  Copyright © 2017 SwifterSwift
+//
+
+import Foundation
+
+// MARK: - Properties
+public extension URL {
+    
+    /// SwifterSwift: Dictionary of the URL's query parameters
+    public var queryParameters: [String: String]? {
+        guard let components = URLComponents(url: self, resolvingAgainstBaseURL: false), let queryItems = components.queryItems else { return nil }
+        
+        var items: [String: String] = [:]
+        
+        for queryItem in queryItems {
+            items[queryItem.name] = queryItem.value
+        }
+        
+        return items
+    }
+}
+
+// MARK: - Methods
+public extension URL {
+	
+	/// SwifterSwift: URL with appending query parameters.
+	///
+	///		let url = URL(string: "https://google.com")!
+	///		let param = ["q": "Swifter Swift"]
+	///		url.appendingQueryParameters(params) -> "https://google.com?q=Swifter%20Swift"
+	///
+	/// - Parameter parameters: parameters dictionary.
+	/// - Returns: URL with appending given query parameters.
+	public func appendingQueryParameters(_ parameters: [String: String]) -> URL {
+		var urlComponents = URLComponents(url: self, resolvingAgainstBaseURL: true)!
+		var items = urlComponents.queryItems ?? []
+		items += parameters.map({ URLQueryItem(name: $0, value: $1) })
+		urlComponents.queryItems = items
+		return urlComponents.url!
+	}
+	
+	/// SwifterSwift: Append query parameters to URL.
+	///
+	///		var url = URL(string: "https://google.com")!
+	///		let param = ["q": "Swifter Swift"]
+	///		url.appendQueryParameters(params)
+	///		print(url) // prints "https://google.com?q=Swifter%20Swift"
+	///
+	/// - Parameter parameters: parameters dictionary.
+	public mutating func appendQueryParameters(_ parameters: [String: String]) {
+		self = appendingQueryParameters(parameters)
+	}
+
+}
